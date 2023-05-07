43ff0d5f3735a005320c256ed6a9b3d4fd058bd0
Felix7854 <Felix.Hagan@amalitech.org>
2023-05-07 19:14:52 +0000

    modified files and added a new image

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-    let  todoList = ['Cook some rice','Prepare stew'];
-
-    $('#new-input').on('keypress',function(e){
-        e.preventDefault();
-        if(e.which == 13){
+    let  todoList = ['cook with me'];
+    let selectedItem;
+    setHtml();
+    $('#new-input').on('keypress',function(event){
+        
+        if(event.which == 13){
             const enteredValue = this.value;
             todoList.push(enteredValue);
             setHtml();
+            clearInput();
+            event.preventDefault();
         }
+    })
+
+    $('.todo-item').hover(function(event) {
+      $('.remove-item').css('display','block');
+      event.preventDefault();
     })
 
     function setHtml(){
@@ -23,13 +31,19 @@ $(document).ready(function(){
               <div class="todo-texts">
                ${val}
               </div>
+              <div class="remove-item" id="${index}"><img src="./images/removeButton.png"></div>
             </div>` 
         });
-        debugger;
+        // debugger;
+
+        $('#itemLeft').html(todoList.length);
     
         $('#todo-items').html(text);
     
     }
-   
+
+    function clearInput(){
+      $('#new-input').val('');
+    }
 })
 
